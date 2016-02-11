b97577682b75f07eede440922a700ffd3bf9c26b
Fenda Truong <ftruong@etech.as.ua.edu>
2016-02-10 19:59:35 -0500

    Added get/post functionality, now working with twig

--- functions.php
+++ functions.php
@@ -1,15 +1,48 @@
 <?php
-require 'config.php';
 // generate a random integer for the main page
 function gen_uid($l=9){
     return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyz"), 0, $l);
 }
 
-/* Updating the Database */
 
-function getConnection() {
-    global $dbhost, $dbuser, $dbpass, $dbname;
-    $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $dbh;
+function addText(){
+    $request = Slim::getInstance()->request();
+    $text = json_decode($request->getBody());
+    $sql = "INSERT INTO textpad ()";
+}
+
+
+/* Generic CRUD functions */
+class Database
+{
+     
+    private static $cont  = null;
+     
+    public function __construct() {
+        die('Init function is not allowed');
+    }
+     
+    public static function connect()
+    {
+        
+       global $dbName, $dbHost, $dbUsername, $dbUserPassword;
+       // One connection through whole application
+       if ( null == self::$cont )
+       {     
+        try
+        {
+          self::$cont =  new PDO( "mysql:host=".$dbHost.";"."dbname=".$dbName, $dbUsername, $dbUserPassword); 
+        }
+        catch(PDOException $e)
+        {
+          die($e->getMessage()); 
+        }
+       }
+       return self::$cont;
+    }
+     
+    public static function disconnect()
+    {
+        self::$cont = null;
+    }
 }
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-require 'vendor/autoload.php';
+require 'includes/vendor/autoload.php';
 require 'functions.php';
+require 'config.php';
 
 /* debugging data */
 $configuration = [
@@ -13,17 +14,65 @@ $c = new \Slim\Container($configuration);
 /* actual app */
 $app = new \Slim\App($c);
 
-$app->get('/', function ($request, $response, $args) {
+/* Twig Settings */
+$container = $app->getContainer();
+
+// Register Twig View helper and configure it
+$container['view'] = function ($c) {
+    //You can change this as you want
+    $view = new \Slim\Views\Twig('templates', [
+        'cache' => false //or specify a cache directory
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $view->addExtension(new Slim\Views\TwigExtension(
+        $c['router'],
+        $c['request']->getUri()
+    ));
+
+    return $view;
+};
+
+$app->get('/', function($request, $response, $args) {
     $gen_uid = gen_uid();
     return $response->withRedirect('/' . $gen_uid);
 });
 
-$app->get('/{gen_uid}', function ($request, $response, $args) {
-    ?>
-    <textarea>sup</textarea>
-    <?php
-    getConnection();
-    //return $response->write("Hello " . $args['gen_uid']);
+$app->get('/{gen_uid}', function($request, $response, $args) {
+    // get from database if exists
+    $pdo = Database::connect();
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    $sql = 'SELECT user_text FROM textpad WHERE gen_uid=?';
+    $q = $pdo->prepare($sql);
+    $q->execute(array($args['gen_uid']));
+    $note_details = $q->fetch();
+    Database::disconnect();
+    
+    return $this->view->render($response, 'note-template.twig', ['gen_uid' => $args['gen_uid'],
+     'user_text' => $note_details['user_text']]);
 });
 
+$app->post('/{gen_uid}', function($request, $response, $args) {
+    // add to database
+    $pdo = Database::connect();
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    $sql = "INSERT INTO textpad (gen_uid, user_text, last_updated) VALUES (?, ?, CURDATE()) ON DUPLICATE KEY UPDATE user_text=?, last_updated=CURDATE()";
+    $q = $pdo->prepare($sql);
+    
+    // get content from note-template.twig
+    $post = $request->getParsedBody();
+    $user_text = $post['user_text']; 
+    $q->execute(array($args['gen_uid'], $user_text, $user_text));
+    Database::disconnect();
+
+    return $this->view->render($response, 'note-template.twig', ['gen_uid' => $args['gen_uid'], 'user_text' => $user_text]);
+
+    // TODO: don't add if there's no content in the user_text
+    // TODO: on refresh, get rid of the alert (am I doing something improperly?)
+    // TODO: related -- refer to GET method instead of return!
+});
+
+
+// Run app
 $app->run();
+
